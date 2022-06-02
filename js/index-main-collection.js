58b6182b10330b0360ae8c704c5219fc1c832a3a
yura <ukoval400@gmail.com>
2022-06-03 00:15:33 +0300

    pull comit

--- js/index-main-collection.js
+++ js/index-main-collection.js
@@ -1,7 +1,7 @@
 //console.log()
 const tabsMain = document.querySelectorAll('[data-tab]');
-const contentBoxes = document.querySelectorAll('[data-tab-content]')
-
+const contentBoxes = document.querySelectorAll('[data-tab-content]');
+//const line = document.querySelectorAll('[data-tab]');
 
 
 tabsMain.forEach(function (item) {
@@ -10,9 +10,16 @@ tabsMain.forEach(function (item) {
 		contentBoxes.forEach(function (item) {
 			item.classList.add('hiden')
 		});
+
+
+
 		const contentBox = document.querySelector('#' + this.dataset.tab);
 		contentBox.classList.remove('hiden');
 
+
+
+
 	})
 })
+
 
