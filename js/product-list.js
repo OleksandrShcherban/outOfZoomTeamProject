8aa733b80f3cda4ee162a533d2e6d405a5f7105d
OlhaBond2021 <olgabondarenko.ua.kh@gmail.com>
2022-06-02 10:58:50 +0000

    add product-list.js and delete some html in sale section

---
+++ js/product-list.js
@@ -0,0 +1,23 @@
+(async function () {
+    function renderProducts(products) {
+        const productsContainer = document.querySelector('.sale__cards');
+        for (const product of products) {
+            productsContainer.innerHTML += `
+                <div class="sale__card">
+                    <a href="trainers.html">
+                        <img class="sale__card-img" src="${product.imgUrl}" alt="${product.name}">
+                    </a>
+                    <a href="trainers.html" class="sale__card-link">${product.name}</a>
+                    <span class="sale__card-price">${product.price}</span>
+                    <input class="base__button" type="submit" value="Add to Cart">
+                </div>
+            `;
+        }
+    }
+
+    const response = await fetch('products.json');
+    const products = await response.json();
+
+    renderProducts(products);
+
+})();
