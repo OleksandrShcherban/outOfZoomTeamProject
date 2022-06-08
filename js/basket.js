6124734c1704608ea37cf15821aa2a016f277b64
OlhaBond2021 <olgabondarenko.ua.kh@gmail.com>
2022-06-08 11:12:23 +0000

    add basket.js

---
+++ js/basket.js
@@ -0,0 +1,20 @@
+(function() {
+
+    const basket = `<div class="basket-show basket__wrapper">
+                            <div class="basket__svg-bg">
+                                <div class="basket__circle-with-number">
+                                    <span class="basket__quantity-of-goods">1</span>
+                                </div>
+                            </div>
+                        </div>`;
+
+    function renderBasketAfterClick() {
+        const basketShow = document.querySelector('.basket__container');
+        basketShow.innerHTML += basket;
+    }
+
+    const btnAddGoodsInBasket = document.querySelector('.sale__card .base__button');
+    btnAddGoodsInBasket.addEventListener('click', renderBasketAfterClick);
+
+
+})();
